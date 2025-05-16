67c1816d2b825610e4fe4f501f2a6e6fb3321d6c
Alexander James Phillips <ajp.global@gmail.com>
2025-05-16 17:12:25 +0100

    Add id_regexs test to list

--- src/tests.ts
+++ src/tests.ts
@@ -2,11 +2,14 @@
 // TODO: implement some way to run all tests (also add some more tests :-))
 // Perhaps explore using https://github.com/kvendrik/jest-lite to run the tests
 
-import { test_partition_items_by_created_at_datetime } from "./shared/utils_datetime/utils_datetime.test"
+import { test_partition_items_by_created_at_datetime } from "./shared/utils_datetime/utils_datetime.test";
+import { test_get_ids_from_text, test_id_regexs } from "./sharedf/rich_text/id_regexs.test";
 
 
 // in the browser and in automated tests for running in this like Github actions
 export async function run_all_tests()
 {
     ;(await test_partition_items_by_created_at_datetime)()
+    ;(await test_id_regexs)()
+    ;(await test_get_ids_from_text)()
 }
